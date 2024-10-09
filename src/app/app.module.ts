5db12df841daf3bd652077ccdcf2a6e1c9dc8722
tonovarela <tonovarela@live.com>
2024-10-08 19:22:18 -0600

    secciones 1 a la 4 maquetados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TituloSeccionComponent } from './components/titulo-seccion/titulo-seccion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TituloSeccionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/titulo-seccion/titulo-seccion.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo-seccion',
+  templateUrl: './titulo-seccion.component.html',  
+})
+export class TituloSeccionComponent {
+
+  @Input('titulo') titulo: string=" ";
+  @Input('numero') numero: string=" ";
+  @Input('departamento') departamento: string=" ";
+
+
+}
